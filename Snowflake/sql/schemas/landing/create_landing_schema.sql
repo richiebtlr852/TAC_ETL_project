f3ea8bf592b57d4eab5182317ba62bc487f8020d
richiebtlr852 <94606168+richiebtlr852@users.noreply.github.com>
2024-12-20 12:30:25 +1100

    Update create_landing_schema.sql

--- Snowflake/sql/schemas/landing/create_landing_schema.sql
+++ Snowflake/sql/schemas/landing/create_landing_schema.sql
@@ -1,1 +1,12 @@
+-- Create the database if it does not already exist
+CREATE DATABASE IF NOT EXISTS tac_landing;
+
+-- Use the database
+USE DATABASE tac_landing;
+
+-- Create the landing schema if it does not already exist
 CREATE SCHEMA IF NOT EXISTS landing;
+
+-- Verify the schema creation
+SHOW SCHEMAS LIKE 'landing';
+
